13cef407a0b3512fe1e2e2763b8b515981469ffc
Jeongjaew0n1 <qlgmq12@gmail.com>
2025-09-23 11:05:22 +0900

    feat.20250923

--- cardatabase/src/main/java/com/example/cardatabase/domain/Car.java
+++ cardatabase/src/main/java/com/example/cardatabase/domain/Car.java
@@ -1,9 +1,6 @@
 package com.example.cardatabase.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -14,17 +11,84 @@ public class Car {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
     private String brand, model, color, registrationNumber;
-
     private int modelYear, price;
 
-    public Car(String brand, String model, String color, String registrationNumber, int modelYear, int price) {
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "owner")
+    private Owner owner;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+
+    public void setModelYear(int modelYear) {
+        this.modelYear = modelYear;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public int getModelYear() {
+        return modelYear;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public Car(String brand, String model, String color, String registrationNumber, int modelYear, int price, Owner owner) {
         this.brand = brand;
         this.model = model;
         this.color = color;
         this.registrationNumber = registrationNumber;
         this.modelYear = modelYear;
         this.price = price;
+        this.owner = owner;
     }
 }
--- cardatabase/src/main/java/com/example/cardatabase/domain/Owner.java
+++ cardatabase/src/main/java/com/example/cardatabase/domain/Owner.java
@@ -1,7 +1,28 @@
 package com.example.cardatabase.domain;
 
+import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import java.util.List;
 
+@NoArgsConstructor(force = true)
+@RequiredArgsConstructor
 @Data
+@Entity
 public class Owner {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long ownerId;
+
+    @NonNull
+    private  String firstName;
+
+    @NonNull
+    private String lastName;
+
+    // 소유자는 다수의 차들을 가질 수 있기 때문에 Collections를 사용
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
+    private List<Car> cars;
 }
